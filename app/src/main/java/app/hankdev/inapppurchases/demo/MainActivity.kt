16841e7dcc6c56f71393a38edc72638358f48012
Hank Li <HankLi0130@gmail.com>
2025-03-20 11:11:37 +0800

    Updated MainActivity.kt

--- app/src/main/java/app/hankdev/inapppurchases/demo/MainActivity.kt
+++ app/src/main/java/app/hankdev/inapppurchases/demo/MainActivity.kt
@@ -1,16 +1,22 @@
 package app.hankdev.inapppurchases.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.hankdev.inapppurchases.demo.ui.theme.InAppPurchasesDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +26,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             InAppPurchasesDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("InAppPurchasesDemo")
+                        Spacer(Modifier.height(8.dp))
+                        Button(::buy) {
+                            Text("Buy")
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InAppPurchasesDemoTheme {
-        Greeting("Android")
+    private fun buy() {
+        Log.i("hank", "Buy")
     }
 }
